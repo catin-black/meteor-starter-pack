2a384047b3c4ccf5f86da7560953853866ec4cfd
Tomasz Nolberczak <tomasz.nolberczak@czarnykod.pl>
2016-06-20 12:14:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Installing & configuring ESLint
+
+Read this tutorial: https://medium.com/@dan_abramov/lint-like-it-s-2015-6987d44c5b48#.jaqua2hc0
+and this: http://guide.meteor.com/code-style.html#eslint-installing
---
+++ imports/client/main.js
@@ -0,0 +1,10 @@
+// Routes
+import './lib/routes.js';
+
+// Helpful things
+import './utils/functions.js';
+import './utils/helpers.js';
+
+// Partials
+import './views/partials/header/header.js';
+import './views/partials/footer/footer.js';
